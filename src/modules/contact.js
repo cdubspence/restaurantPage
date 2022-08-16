ea47bc70fbf788bfa66cdf2915d910ed6c0b121c
Casey Spence <orangobang@gmail.com>
2022-08-15 20:30:11 -0400

    updating stlye

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -2,10 +2,10 @@ export function loadContactPage() {
     const content = document.getElementById('content');
     
     const contact = document.createElement('div');
-    contact.className = 'contact;'
+    contact.className = 'contact'
     contact.innerHTML = `
     <h1 class='title'>We want to hear from you!</h1>
-    <form>
+    <form id='contactForm'>
     Name: <input type='text'>
     Email: <input type='email'>
     Comments: <input type='textarea'>
